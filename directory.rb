e7fc55bf5ff4ac4dd844b48484565b47c9c63dc7
pauladarias <pauladarias@gmail.com>
2020-04-07 11:46:26 +0100

    loading data from the file

--- directory.rb
+++ directory.rb
@@ -1,8 +1,17 @@
 @students = []
 
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
 def save_students
   # open the file for writing
-  file = File.open("student-directory/students.csv", "w")
+  file = File.open("students.csv", "w")
   # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
@@ -15,14 +24,10 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    # get another name from the user
     name = gets.chomp
   end
 end
@@ -38,6 +43,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -55,14 +61,16 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
-    exit # this will cause the program to terminate
+    exit
   else
     puts "I don't know what you meant, try again"
   end
 end
 
-@students = [
+students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Nurse Ratched", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -82,7 +90,6 @@ def print_header
 end
 
 def print_students_list
-###print number before students
 @students.each_with_index do |student, index|
   indexplusone = index + 1
   puts "#{indexplusone} #{student[:name]} (#{student[:cohort]} cohort)"
@@ -94,6 +101,7 @@ def print_footer
 end
 
 interactive_menu
+#save_students
 
 #
 # def print(students)
